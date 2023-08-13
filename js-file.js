6673f1a396bbf4723ddc9196aa19b1a20ea4c530
Carlos <44456542+chawy11@users.noreply.github.com>
2023-08-13 16:58:16 +0200

    small fixes

--- js-file.js
+++ js-file.js
@@ -1,9 +1,44 @@
-
-document.addEventListener('DOMContentLoaded', function() {
+document.addEventListener('DOMContentLoaded', function () {
     const characters = document.querySelectorAll('.character');
     const startButton = document.getElementById('start-game');
     const characterSelection = document.querySelector('.character-selection');
     const gameContent = document.querySelector('.game-content');
+    const optionsImages = document.querySelectorAll('.options img');
+    const roundNumber = document.getElementById('round-number');
+    const gameResult = document.getElementById('game-result');
+    const playerCharacter = document.getElementById('player-character');
+    const computerCharacter = document.getElementById('computer-character');
+    const resetButton = document.getElementById('reset-button');
+    const playAgainButton = document.getElementById('play-again-button');
+    const playMusicButton = document.getElementById('playMusic');
+    
+
+    let canPlay = true;
+    let totalRounds = 0;
+    let playerScore = 0;
+    let computerScore = 0;
+
+    optionsImages.forEach(image => {
+        image.addEventListener('click', () => {
+            if (canPlay && totalRounds < 5) {
+                canPlay = false;
+
+                const machineChoice = getComputerChoice();
+
+                const computerChoiceImage = document.createElement('img');
+                computerChoiceImage.src = `bison_${machineChoice}.png`;
+                computerChoiceImage.classList.add('selected-image');
+                computerCharacter.appendChild(computerChoiceImage);
+
+                const playerChoiceImage = document.createElement('img');
+                playerChoiceImage.src = image.src;
+                playerChoiceImage.classList.add('selected-image');
+                playerCharacter.appendChild(playerChoiceImage);
+
+                playRound(image.id, machineChoice);
+            }
+        });
+    });
 
     let selectedCharacter = null;
 
@@ -21,111 +56,115 @@ document.addEventListener('DOMContentLoaded', function() {
             gameContent.style.display = 'block';
             const selectedCharacterSmallGif = document.getElementById(selectedCharacter).getAttribute('data-gif-small');
             const selectedCharacterLargeGif = document.getElementById(selectedCharacter).getAttribute('data-gif-large');
-        
+
             const characterLargeImage = document.querySelector('.character-image-large img');
             characterLargeImage.src = selectedCharacterLargeGif;
         } else {
-            alert('Por favor, selecciona un personaje antes de empezar el juego.');
+            alert('Please choose a character before starting the game.');
         }
     });
-
-    let playerWins = 0
-    let computerWins = 0
-    let totalRounds = 0
 
     function winner(playerSelection, computerSelection) {
-        const tieMessage = 'Empatao desgraciao'
-        const loserMessage = 'Perdiste pringao'
-        const winnerMessage = 'Ganaste tryharder'
+        const tieMessage = 'Tie';
+        const loserMessage = 'You lose';
+        const winnerMessage = 'You win';
         console.log('Computer chose:', computerSelection);
+
         if (playerSelection === computerSelection) {
-            alert(tieMessage)
-        }
-        if (playerSelection === 'paper') {
-            if (computerSelection === 'scissors') {
-                alert(loserMessage)
-                computerWins = +1
-            }
-            if (computerSelection === 'rock') {
-                alert(winnerMessage)
-                playerWins = +1
-            }
-        }
-        if (playerSelection === 'scissors') {
-            if (computerSelection === 'rock') {
-                alert(loserMessage)
-                computerWins = +1
-            }
-            if (computerSelection === 'paper') {
-                alert(winnerMessage)
-                playerWins = +1
-            }
-        }
-        if (playerSelection === 'rock') {
-            if (computerSelection === 'paper') {
-                alert(loserMessage)
-                computerWins = +1
-            }
-            if (computerSelection === 'scissors') {
-                alert(winnerMessage)
-                playerWins = +1
-            }       
+            alert(tieMessage);
+        } else if (
+            (playerSelection === 'rock' && computerSelection === 'scissors') ||
+            (playerSelection === 'paper' && computerSelection === 'rock') ||
+            (playerSelection === 'scissors' && computerSelection === 'paper')
+        ) {
+            alert(winnerMessage);
+            playerScore++;
+        } else {
+            alert(loserMessage);
+            computerScore++;
         }
     }
-
 
     function getComputerChoice() {
-        const choices = ['rock', 'paper', 'scissors']
-        const randomIndex = Math.floor (Math.random() * choices.length);
-        return choices[randomIndex]
+        const choices = ['rock', 'paper', 'scissors'];
+        const randomIndex = Math.floor(Math.random() * choices.length);
+        return choices[randomIndex];
     }
 
-    function playRound(playerSelection) {
-        const computerSelection = getComputerChoice();
-        winner(playerSelection, computerSelection);
+    function playRound(playerSelection, machineSelection) {
+        winner(playerSelection, machineSelection);
         totalRounds++;
 
+        roundNumber.textContent = `${playerScore}-${computerScore}`;
+
         if (totalRounds === 5) {
-            showWinner(); // After 5 rounds, announce the winner
-            resetGame();
+            showWinner();
+            canPlay = false;
+        } else {
+            canPlay = true;
         }
     }
-
 
     function showWinner() {
-        if (playerWins > computerWins) {
-            console.log('Player wins', 'player:', playerWins);
-        } else if (computerWins > playerWins) {
-            console.log('Computer wins', 'computer:', computerWins);
+        let resultMessage = '';
+        if (playerScore > computerScore) {
+            resultMessage = 'You Win';
+        } else if (computerScore > playerScore) {
+            resultMessage = 'You Lose';
         } else {
-            console.log('It\'s a tie');
+            resultMessage = 'It\'s a Tie';
         }
+        gameResult.textContent = resultMessage;
+        gameResult.style.display = 'block';
+        resetButton.style.display = 'block';
+        playAgainButton.style.display = 'block';
     }
 
-    function resetGame() {
-        playerWins = 0;
-        computerWins = 0;
-        totalRounds = 0;
-    }
-
-    document.getElementById('rock').addEventListener('click', function() {
-        playRound('rock');
+    resetButton.addEventListener('click', () => {
+        resetGame();
     });
 
-    document.getElementById('paper').addEventListener('click', function() {
-        playRound('paper');
+    playAgainButton.addEventListener('click', () => {
+        playAgain();
     });
 
-    document.getElementById('scissors').addEventListener('click', function() {
-        playRound('scissors');
-    });
 
-    document.getElementById('playMusic').addEventListener('click', function() {
+    playMusicButton.addEventListener('click', function () {
         const backgroundMusic = document.getElementById('backgroundMusic');
         backgroundMusic.play();
     });
 
+
+    function resetGame() {
+        playerScore = 0;
+        computerScore = 0;
+        totalRounds = 0;
+        canPlay = true;
+        roundNumber.textContent = '0-0';
+        gameResult.style.display = 'none';
+        resetButton.style.display = 'block';
+        playAgainButton.style.display = 'block';
+
+        playerCharacter.innerHTML = '';
+        computerCharacter.innerHTML = '';
+    }
+
+    function playAgain() {
+        resetGame();
+
+        characterSelection.style.display = 'block';
+        gameContent.style.display = 'none';
+    }
 });
+
+
+
+
+
+
+
+
+
 
         
     
